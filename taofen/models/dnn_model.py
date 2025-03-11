5f0f9101a32652bcd2a94deacff699d7b06ebc7e
tangweize <tangweize@tuhu.cn>
2025-03-11 19:40:44 +0800

    just

--- taofen/models/dnn_model.py
+++ taofen/models/dnn_model.py
@@ -1,25 +1,19 @@
-# Author: tangweize
-# Date: 2025/3/10 17:52
-# Description: 
-# Data Studio Task:
-
 import tensorflow as tf
 from tensorflow.keras import layers, Model
+from tensorflow import keras
 
-import tensorflow as tf
-from tensorflow.keras import layers, Model
-
-
-def Custom_Model(Model):
+# 正则项 loss之和：  self.losses
+# model 类的 loss 损失函数 
+class Custom_Model(Model):
     def __init__(self, preprocess_model, units):
         super().__init__()
         self.preprocess_model = preprocess_model
         self.dnn = keras.Sequential()
         for unint in units:
             self.dnn.add(
-                layers.Dense(unint, activation='relu'
-                             )
-            self.dnn.add(layers.Dense(1))
+                layers.Dense(unint, activation='relu')
+            )
+        self.dnn.add(layers.Dense(1))
 
     def call(self, inputs):
         x = self.preprocess_model(inputs)
@@ -27,10 +21,15 @@ def Custom_Model(Model):
         return x
 
     def tain_step(self, inputs, label):
+
+
         with tf.GradientTape() as tape:
             predict = self(inputs)
-            self.loss
+            losses = self.loss(label,predict)
 
+        trainable_vars = self.trainable_vars
+        gradients = tape.GradientTape(losses, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-
-
+        results = {m.name: m.result() for m in self.metrics}
+        return results
