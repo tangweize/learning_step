f4c689ec36ada5a8e0714773d6b59a101f7b3300
tangweize <756648174@qq.com>
2025-06-29 10:26:29 +0800

    just conmit

--- 25_Q3_multi_window_ltv_model/models/model.py
+++ 25_Q3_multi_window_ltv_model/models/model.py
@@ -150,7 +150,7 @@ class DNN(layers.Layer):
     def call(self, x):
         return self.dnn(x)
 
-
+# 正式模型
 class MULTI_HEAD_LTV_MODEL(keras.Model):
     def __init__(self,
                  num_heads,
@@ -370,7 +370,7 @@ class MULTI_HEAD_LTV_MODEL(keras.Model):
         return area_pred
 
 
-
+# MMOE
 class MMOE(keras.Model):
     def __init__(self,
                  num_heads,
@@ -490,3 +490,244 @@ class MMOE(keras.Model):
                 hour_model_true[head] += tf.reduce_sum(head_true)
 
         return hour_model_pred, hour_model_true
+
+
+"""
+demo model 之前用keras 不做任何处理效果都不差。
+将复现一下，寻找其中diff
+"""
+
+class NO_Process_Layer(layers.Layer):
+    def __init__(self, dense_cnt_features, dense_price_features, dense_duration_features, user_sparse_features):
+        super().__init__()
+
+        self.features = [dense_cnt_features
+                        ,dense_price_features
+                        , dense_duration_features
+                        ,user_sparse_features
+                         ]
+        self.concat_layer = layers.Concatenate()
+    def call(self, inputs):
+        processed_dense_features = []
+        for field, input_tensor in inputs.items():
+            if field in self.features:
+                input_tensor = tf.cast(input_tensor, tf.float32)  # 显式转 float
+                processed_dense_features.append(input_tensor)
+        return self.concat_layer(processed_dense_features)
+
+
+class Single_HEAD_LTV_MODEL(keras.Model):
+    def __init__(self,
+                 num_heads,
+                 units,
+                 head_units,
+                 dense_cnt_feature_name,
+                 dense_price_feature_name,
+                 dense_duration_feature_name,
+                 sparse_group_name,
+                 user_sparse_features,
+                 emb_features,
+                 hour_flag = 'request_hour_diff',
+                 is_log = True):
+        super().__init__()
+        self.num_heads = num_heads
+
+        self.process_dense_layer = NO_Process_Layer(dense_cnt_feature_name, dense_price_feature_name, dense_duration_feature_name, sparse_group_name)
+
+        self.sparse_features = user_sparse_features
+        self.sparse_group_name = sparse_group_name
+        self.hour_flag = hour_flag
+
+        self.sharebottom = tf.keras.Sequential([
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(1)
+        ])
+
+        # Each head as a Sequential model
+        # self.hour2headnn = [ tf.keras.layers.Dense(1) for _ in range(self.num_heads)
+        # ]
+        self.concat_layer = layers.Concatenate()
+    def call(self, inputs, training=False):
+        input_tensor = self.process_dense_layer(inputs)
+        print(input_tensor.shape)
+        sharebottom_out = self.sharebottom(input_tensor)
+
+        # outputs = [head(sharebottom_out) for head in self.hour2headnn]
+
+
+        # hour_idx = -1
+        # for i, v in enumerate(self.sparse_features):
+        #     if v == self.hour_flag:
+        #         hour_idx = tf.cast(tf.gather(inputs[self.sparse_group_name], indices=i, axis = 1) - 1, tf.int64)
+        #         hour_idx = tf.clip_by_value(hour_idx, 0, self.num_heads - 1)
+
+        # outputs = self.concat_layer(outputs)
+        # selected = tf.gather(outputs, hour_idx, axis=1, batch_dims=1)
+        # selected = tf.expand_dims(selected, axis=1)  # 模拟 keep_dim=True 的效果
+
+
+        return sharebottom_out
+
+    def train_step(self, train_data):
+        inputs, label = train_data
+        with tf.GradientTape() as tape:
+            predict = self(inputs)
+            losses = self.loss(label, predict)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(losses, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.compiled_metrics.update_state(label, predict)
+        results = {m.name: m.result() for m in self.metrics}
+        return results
+
+    def predict(self, inputs, batch_size=None):
+        return super().predict(inputs, batch_size=batch_size)
+
+
+    # 存储 每条样本的 pred 和 true，并分别保存为各个head；
+    # 返回： pred:[],  true:[]
+    def predict_head_score(self, test_dataset):
+        hour_model_pred = {k: [] for k in range(self.num_heads)}
+        hour_model_true = {k: [] for k in range(self.num_heads)}
+        mode = self.loss.mode
+        for batch in test_dataset:
+            inputs, y_true_packed = batch
+            pred = self(inputs)
+
+            hour_idx = None
+
+            if mode in ('delta', 'log_delta') :
+                y_pred = pred + tf.reshape(y_true_packed[:, 1], (-1, 1))  # shape (B, 1)
+                y_true = tf.reshape(y_true_packed[:, 0], (-1, 1))
+
+            elif mode in ['mse', 'mae', 'mape', 'log']:
+                y_pred = pred
+                y_true = tf.reshape(y_true_packed[:, 0], (-1, 1))
+            elif mode in ['binary']:
+                y_pred = tf.sigmoid(pred)
+                y_true = tf.reshape(y_true_packed[:, 0], (-1, 1))
+
+
+            else:
+                raise ValueError(f"Unsupported mode in evaluation: {mode}")
+
+            for i, v in enumerate(self.sparse_features):
+                if v == self.hour_flag:
+                    hour_idx = tf.cast(tf.gather(inputs[self.sparse_group_name], indices=i, axis = 1) - 1, tf.int64)
+                    hour_idx = tf.clip_by_value(hour_idx, 0, self.num_heads - 1)
+
+            for head in range(self.num_heads):
+                idxs = tf.where(tf.equal(hour_idx, head))[:, 0]
+                if tf.size(idxs) == 0:
+                    continue  # 该 head 在当前 batch 没有数据，跳过
+
+                # 选出对应 head 的 pred 和 label
+                # print(y_pred.shape, y_true.shape) # (2048, 1) (432,)
+                head_pred = tf.gather(y_pred, idxs)
+                head_true = tf.gather(y_true, idxs)
+
+                hour_model_pred[head].append(head_pred)
+                hour_model_true[head].append(head_true)
+
+        # 处理成 一维tensor
+        def safe_concat_and_maybe_squeeze(tensor_list):
+            out = tf.concat(tensor_list, axis=0)
+            if len(out.shape) == 2 and out.shape[1] == 1:
+                out = tf.squeeze(out, axis=1)
+            return out
+
+        for head in range(self.num_heads):
+            if len(hour_model_pred[head]) == 0:
+                continue
+
+            hour_model_pred[head] = safe_concat_and_maybe_squeeze(hour_model_pred[head])
+            hour_model_true[head] = safe_concat_and_maybe_squeeze(hour_model_true[head])
+        return hour_model_pred, hour_model_true
+    def evaluate_exp(self, test_dataset):
+        # 计算各个头的期望的 bias
+
+        head_pred, head_true = self.predict_head_score(test_dataset)
+
+        mape = {}
+        for head in head_pred.keys():
+            # 对每个head的所有结果 求和
+            head_pred[head] = tf.reduce_sum(head_pred[head]).numpy()
+            head_true[head] = tf.reduce_sum(head_true[head]).numpy()
+
+
+            pred = head_pred[head]
+            true = head_true[head]
+            mape[head] = (pred - true) / (true + 1.0)
+            mape[head] = round(mape[head], 2)
+
+
+        return {
+            'pred_sum': head_pred,
+            'true_sum': head_true,
+            'bias' : mape
+        }
+
+
+
+
+    def evaluate_rank(self, test_dataset):
+
+        head_pred, head_true = self.predict_head_score(test_dataset)
+
+        rank_res = {}
+        for head in head_pred.keys():
+            head_name = f"{head + 1}_h rank_score"
+            pred = head_pred[head]
+            true = head_true[head]
+            if len(pred):
+                rank_res[head_name] = self.calculate_area_under_gain_curve(pred, true, head_name)
+
+        return rank_res
+
+    def calculate_area_under_gain_curve(self, pred_list, true_list, head_name=""):
+        # 将零维张量列表转换为一维 NumPy 数组
+        pred = pred_list.numpy()
+        true = true_list.numpy()
+
+        # 创建 DataFrame
+        df = pd.DataFrame({'pred': pred, 'true': true})
+
+        # 【1】预测值排序的增益曲线
+        df_pred_sorted = df.sort_values(by='pred', ascending=False).copy()
+        df_pred_sorted['cumulative_percentage_customers'] = np.arange(1, len(df_pred_sorted) + 1) / len(df_pred_sorted)
+        df_pred_sorted['cumulative_percentage_ltv'] = df_pred_sorted['true'].cumsum() / df_pred_sorted['true'].sum()
+        area_pred = np.trapz(df_pred_sorted['cumulative_percentage_ltv'],
+                             df_pred_sorted['cumulative_percentage_customers'])
+
+        # 【2】真实值排序的理想增益曲线（Ground Truth 理想线）
+        df_true_sorted = df.sort_values(by='true', ascending=False).copy()
+        df_true_sorted['cumulative_percentage_customers'] = np.arange(1, len(df_true_sorted) + 1) / len(df_true_sorted)
+        df_true_sorted['cumulative_percentage_ltv'] = df_true_sorted['true'].cumsum() / df_true_sorted['true'].sum()
+        area_true = np.trapz(df_true_sorted['cumulative_percentage_ltv'],
+                             df_true_sorted['cumulative_percentage_customers'])
+
+        # 【3】绘图
+        plt.figure(figsize=(10, 6))
+        plt.plot(df_pred_sorted['cumulative_percentage_customers'],
+                 df_pred_sorted['cumulative_percentage_ltv'],
+                 label="Gain Curve (Predicted)", linewidth=2)
+        plt.plot(df_true_sorted['cumulative_percentage_customers'],
+                 df_true_sorted['cumulative_percentage_ltv'],
+                 label="Ideal Gain Curve (Ground Truth Sorted)",
+                 linestyle='--', color='black', linewidth=2)
+        plt.plot([0, 1], [0, 1], linestyle=':', color='gray', label="Random Model")
+
+        plt.xlabel('Cumulative Percentage of Customers')
+        plt.ylabel('Cumulative Percentage of Total LTV')
+        plt.title(f'{head_name} Gain Chart')
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
+        return area_pred
